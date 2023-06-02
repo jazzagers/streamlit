824f1a08edba2aa5bc891f2a056d970238374763
jazzagers <123601374+jazzagers@users.noreply.github.com>
2023-06-02 10:08:49 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+# https://docs.streamlit.io/library/cheatsheet
 import streamlit as st
 st.title('My CE App') # Sets the the title of your page
 st.header('Data Visualization Section')  # Sets a header for a section
@@ -13,9 +14,28 @@ data = {
 df = pd.DataFrame(data)
 
 st.write(df)
+
 age = st.slider('Your age', min_value=0, max_value=100)  # Returns the value selected by the user
 if st.button('Say Hello'):  # Returns True if the user clicks the button
     st.write('Hello!')
 show_df = st.checkbox('Show DataFrame')  # Returns True if the user checks the box, False otherwise
 if show_df:
     st.write(df)
+
+    
+import pandas as pd
+
+# Creating a pandas DataFrame
+data = {
+    'names': ['John', 'Anna', 'George', 'Mia'],
+    'ages': [23, 45, 37, 19]
+}
+df = pd.DataFrame(data)
+
+import streamlit as st
+
+# Display the DataFrame in Streamlit
+st.dataframe(df)
+
+# Display descriptive statistics of 'ages'
+st.write(df['ages'].describe())
