21a0419520a72a7c6f75320aa8348384fc67fb9c
jazzagers <123601374+jazzagers@users.noreply.github.com>
2023-06-02 10:02:44 -0700

    Update app.py

--- app.py
+++ app.py
@@ -4,9 +4,6 @@ st.header('Data Visualization Section')  # Sets a header for a section
 st.subheader('Subsection: Pie Chart Analysis')  # Sets a subheader for a subsection
 st.text('This section focuses on data preprocessing.')
 st.markdown('**This is some bold text**')
-from Olivia Thorburn to Everyone:    9:59  AM
-Mine is working with the new changes
-from Ernesto Lee to Everyone:    10:00  AM
 import pandas as pd
 
 data = {
